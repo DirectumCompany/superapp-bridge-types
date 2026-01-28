c96c5dd90e98e78c2e294afd09549f92dff67050
Гумеров Тимур (Gumerov_TR) <Gumerov_TR@directum.ru>
2026-01-28 16:27:38 +0500

    rd-1062 Расширил модель

--- invoker.ts
+++ invoker.ts
@@ -12,12 +12,12 @@ export interface IMountResult {
  * Функция монтирования.
  */
 export type MountFunction = (
-  /** Контейнер. */
-  container: HTMLDivElement,
-  /** Имя приложения. */
-  appName: string,
-  /** Свойства монтируемого компонента. */
-  props: object
+    /** Контейнер. */
+    container: HTMLDivElement,
+    /** Имя приложения. */
+    appName: string,
+    /** Свойства монтируемого компонента. */
+    props: object
 ) => IMountResult;
 
 /**
@@ -32,13 +32,56 @@ export interface IRouteInfo {
   children?: Array<IRouteInfo>;
 }
 
-/** 
+/**
  * Параметры инициализации.
  */
 export interface IInitOptions {
   appName: string;
   api: SuperAppBridge;
 }
+
+/** Аргументы выполнения действия. */
+export interface IEntityActionArgs {
+  /** Имя приложения. */
+  appName: string;
+  /** Идентификатор типа сущности. */
+  entityType: string;
+  /** Идентификатор сущности. */
+  entityId: string;
+  /** Идентификатор действия. */
+  actionId: string;
+  /** Параметры действия. */
+  parameters?: Record<string, unknown>;
+}
+
+/** Ошибка действия. */
+export interface IMiniAppError {
+  /** Сообщение ошибки. */
+  message: string;
+  /** Детали ошибки. */
+  details: string;
+}
+
+/** Результат выполнения действия. */
+export interface IEntityActionResult {
+  /** Признак успешности. */
+  isSuccess: boolean;
+  /** Результат валидации. */
+  validationResult?: Array<IValidationMessage>;
+  /** Ошибка. */
+  error?: IMiniAppError;
+}
+
+/** Сообщение об ошибке валидации. */
+interface IValidationMessage {
+  /** Тип ошибки. */
+  type: 'error' | 'warning' | 'information';
+  /** Сообщение об ошибке. */
+  message: string;
+  /** Связанные свойства. */
+  relatedPropertyIds?: Array<string>;
+}
+
 
 /**
  * Интерфейс встраиваемого компонента.
@@ -48,4 +91,8 @@ export interface IRemoteEntry {
   routes: Array<IRouteInfo>;
   /** Функция инициализации компонента. */
   init: (options: IInitOptions) => void;
+  /** Действия компонента. */
+  actions: {
+    executeEntityAction(args: IEntityActionArgs): Promise<IEntityActionResult>;
+  }
 }
